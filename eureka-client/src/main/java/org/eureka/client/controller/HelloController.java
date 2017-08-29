140717f64142e346c67c2bf6789c48b8794e1b87
yongheng.wang <995965744@qq.com>
2017-08-29 23:13:11 +0800

    spring cloud eureka server and client

---
+++ eureka-client/src/main/java/org/eureka/client/controller/HelloController.java
@@ -0,0 +1,19 @@
+package org.eureka.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@Value("${server.port}")
+	private String port;
+	
+	@RequestMapping("/hi/{name}")
+	public String home(@PathVariable String name) {
+		return "hello " + name + ",i am from port:" + port;
+	}
+	
+}
