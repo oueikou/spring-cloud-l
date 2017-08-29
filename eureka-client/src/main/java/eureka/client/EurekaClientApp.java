e2e14655b106d723702ad1486311c89fc1da6232
yongheng.wang <995965744@qq.com>
2017-08-29 23:27:08 +0800

    调整包名

--- eureka-client/src/main/java/eureka/client/EurekaClientApp.java
+++ eureka-client/src/main/java/eureka/client/EurekaClientApp.java
@@ -1,4 +1,4 @@
-package org.eureka.client;
+package eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-client/src/main/java/eureka/client/controller/HelloController.java
+++ eureka-client/src/main/java/eureka/client/controller/HelloController.java
@@ -1,4 +1,4 @@
-package org.eureka.client.controller;
+package eureka.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
